b6fbe03b55cfaa2277a49958d852eabe7190872d
Marian Mocanu <gheo0174@stud.kea.dk>
2019-11-26 21:29:30 +0100

    Added detailed view of an order with editable status

--- src/main/java/dk/kea/stud/fourplayers/restaurantstore/controllers/OrderController.java
+++ src/main/java/dk/kea/stud/fourplayers/restaurantstore/controllers/OrderController.java
@@ -4,8 +4,7 @@ import dk.kea.stud.fourplayers.restaurantstore.order.Order;
 import dk.kea.stud.fourplayers.restaurantstore.order.OrderRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,17 +14,33 @@ public class OrderController {
   private final OrderRepository orders;
 
   private final String ORDERS = "order/orders";
+  private final String ORDER_DETAILS = "order/orderDetails";
 
   public OrderController(OrderRepository orders) {
     this.orders = orders;
   }
 
-  @GetMapping("admin/order/viewAll")
+  @GetMapping("/admin/order/viewAll")
   public String viewAllOrders(Model model) {
     model.addAttribute("orders", orders.findAllByOrderByOrderTimestampDesc());
 
     return ORDERS;
   }
 
-  
+  @GetMapping("/admin/order/view/{orderId}")
+  public String viewOrderDetails(@PathVariable("orderId") int orderId, Model model) {
+    model.addAttribute("order", orders.findById(orderId).get());
+
+    return ORDER_DETAILS;
+  }
+
+  @PostMapping("/admin/order/view/{orderId}")
+  public String updateOrderStatus(@PathVariable("orderId") int orderId,
+                                        @RequestParam("status") Order.Status status) {
+    Order order = orders.findById(orderId).get();
+    order.setStatus(status);
+    orders.save(order);
+    
+    return "redirect:/admin/order/viewAll";
+  }
 }
