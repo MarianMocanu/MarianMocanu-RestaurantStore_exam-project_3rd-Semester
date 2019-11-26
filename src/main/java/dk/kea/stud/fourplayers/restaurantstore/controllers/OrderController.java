017d1a53a4c59acbccf8dfe97332a68fc771eddf
Marian Mocanu <gheo0174@stud.kea.dk>
2019-11-26 18:29:59 +0100

    Added view for all orders

--- src/main/java/dk/kea/stud/fourplayers/restaurantstore/controllers/OrderController.java
+++ src/main/java/dk/kea/stud/fourplayers/restaurantstore/controllers/OrderController.java
@@ -1,13 +1,31 @@
 package dk.kea.stud.fourplayers.restaurantstore.controllers;
 
+import dk.kea.stud.fourplayers.restaurantstore.order.Order;
 import dk.kea.stud.fourplayers.restaurantstore.order.OrderRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 public class OrderController {
   private final OrderRepository orders;
 
+  private final String ORDERS = "order/orders";
+
   public OrderController(OrderRepository orders) {
     this.orders = orders;
   }
+
+  @GetMapping("admin/order/viewAll")
+  public String viewAllOrders(Model model) {
+    model.addAttribute("orders", orders.findAllByOrderByOrderTimestampDesc());
+
+    return ORDERS;
+  }
+
+  
 }
